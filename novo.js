2493aa48d6098dc00f44ae60ab03a658e1c1fdb6
devobservador <samuel.campossilva2023@gmail.com>
2023-12-29 00:09:10 -0300

    atualizações do projeto

--- novo.js
+++ novo.js
@@ -1,1 +1,26 @@
-console.log('salve')
+let nome = 'mari'
+let xp= 5.000
+let nivel= ''
+if(xp<=1.001){
+   nivel= "latao"
+}else if(xp===1.001 || xp===2.000){
+    nivel="ferro"
+
+} else if(xp=== 2.001 ||xp===5.000){
+    nivel= "prata"
+} else if(xp===5.001 || xp=== 7.000){
+    nivel= "ouro"
+} else if(xp===7.001|| xp===8.000){
+    nivel = "platina"
+
+} else if(xp===8.001 || xp===9.000){
+    nivel= "ascendente"
+
+} else if(xp===9.001|| xp===10.000){
+    nivel= "imortal"
+}  else if(xp>=10.001){
+    nivel= "radiante"
+}
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+
